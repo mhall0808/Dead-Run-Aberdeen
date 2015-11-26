86199c1147eb395539619f7e351aa07d3ab524c4
Mark Hall <mhall0717@yahoo.com>
2015-11-26 14:47:17 -0700

    Initial file add
    Created the initial C# files.

---
+++ Inventory/Item/Item.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+	private string itemName;
+	private string itemDescription;
+
+	private int itemHP;
+	private int itemDef;
+	private int itemAttack;
+
+	// Use this for initialization
+	void Start () {
+		itemName = "Default";
+		itemDescription = "Default Description";
+
+		itemHP = 0;
+		itemDef = 0;
+		itemAttack = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private string playerName;
+	private Inventory inventory;
+
+	private int playerHP;
+	private int playerDef;
+	private int playerAttack;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
